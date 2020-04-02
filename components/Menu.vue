6ad10a0504f346dc647b28ac4c17808368f3ddc7
José Gilberto Mullor <gilber@gmail.com>
2020-04-02 16:52:31 +0200

    WIP

--- components/Menu.vue
+++ components/Menu.vue
@@ -3,9 +3,9 @@
         <div class="container">
             <div class="nk-navbar-wrap">
                 <div class="nk-navbar-logo logo">
-                    <a href="./" class="logo-link">
+                    <nuxt-link class="logo-link" to="/">
                         <img src="images/logo-rescue.png" alt="Rescueapp logo" />
-                    </a>
+                    </nuxt-link>
                 </div>
                 <div class="nk-navbar-toggle d-lg-none">
                     <a href="#" class="toggle" data-menu-toggle="navbar-menu">
@@ -16,30 +16,15 @@
                 <nav class="nk-navbar-menu" id="navbar-menu">
                     <ul class="nk-menu">
                         <li class="nk-menu-item">
-                            <a class="scrollto nav-link nk-menu-link" href="#home">Home</a>
+                            <nuxt-link class="nav-link nk-menu-link" to="/">Home</nuxt-link>
                         </li>
                         <li class="nk-menu-item">
-                            <a class="scrollto nav-link nk-menu-link" href="#about">About Corona</a>
-                        </li>
-                        <li class="nk-menu-item">
-                            <a class="scrollto nav-link nk-menu-link" href="#symptoms">Symptoms</a>
-                        </li>
-                        <li class="nk-menu-item">
-                            <a class="scrollto nav-link nk-menu-link" href="#prevention">Prevention</a>
-                        </li>
-                        <li class="nk-menu-item">
-                            <a class="scrollto nav-link nk-menu-link" href="#treatment">Treatment</a>
-                        </li>
-                        <li class="nk-menu-item">
-                            <a class="scrollto nav-link nk-menu-link" href="#faq">FAQ</a>
-                        </li>
-                        <li class="nk-menu-item">
-                            <a class="scrollto nav-link nk-menu-link" href="#news">News</a>
+                            <nuxt-link class="nav-link nk-menu-link" to="/">¿Quienes somos?</nuxt-link>
                         </li>
                     </ul>
                     <ul class="nk-menu-btns">
                         <li class="nk-menu-item">
-                            <a href="#protect" class="btn btn-sm scrollto nav-link">Colabora</a>
+                            <nuxt-link class="btn btn-sm scrollto nav-link" to="/participar">Participar</nuxt-link>
                         </li>
                     </ul>
                 </nav>
---
+++ components/footer.vue
@@ -0,0 +1,85 @@
+<template>
+    <footer class="nk-footer bg-dark tc-light has-overlay">
+        <div class="overlay shape shape-c"></div><!-- Overlay Shape -->
+        <section class="section section-footer section-m tc-light">
+            <div class="container">
+                <div class="nk-footer-top">
+                    <div class="row g-gs gy-m">
+                        <div class="col-lg-3 col-md-9 mr-auto">
+                            <div class="wgs wgs-about">
+                                <div class="wgs-logo logo">
+                                    <a href="./" class="logo-link">
+                                        <img src="images/logo-white.png" srcset="images/logo-white2x.png 2x" alt="logo">
+                                    </a>
+                                </div>
+                                <div class="wgs-about-text">
+                                    <p>This website is for health information and advice about coronavirus (COVID-19), how to prevent and protect yourself from disease.</p>
+                                    <p>Learn about the government response to coronavirus on GOV.UK.</p>
+                                </div>
+                                <ul class="wgs-social">
+                                    <li><a href="#"><em class="icon ni ni-facebook-f"></em></a></li>
+                                    <li><a href="#"><em class="icon ni ni-twitter"></em></a></li>
+                                    <li><a href="#"><em class="icon ni ni-youtube-fill"></em></a></li>
+                                </ul>
+                            </div><!-- .wgs -->
+                        </div><!-- .col -->
+                        <div class="col-sm-4 col-lg-2">
+                            <div class="wgs wgs-menu">
+                                <h6 class="wgs-title">Quick Link</h6>
+                                <ul class="wgs-links">
+                                    <li><a class="scrollto" href="#symptoms">Symptoms</a></li>
+                                    <li><a class="scrollto" href="#prevention">Prevention</a></li>
+                                    <li><a class="scrollto" href="#protect">Protect Youself</a></li>
+                                    <li><a class="scrollto" href="#faq">FAQs</a></li>
+                                    <li><a class="scrollto" href="#about">About Corona</a></li>
+                                </ul>
+                            </div><!-- .wgs -->
+                        </div><!-- .col -->
+                        <div class="col-sm-4 col-lg-3">
+                            <div class="wgs wgs-menu">
+                                <h6 class="wgs-title">Helpfull link</h6>
+                                <ul class="wgs-links">
+                                    <li><a href="#">Healthcare Professionals</a></li>
+                                    <li><a href="#">Healthcare Facilities</a></li>
+                                    <li><a href="#">Older Adults & Medical Conditions</a></li>
+                                    <li><a href="#">Repare your Family</a></li>
+                                </ul>
+                            </div><!-- .wgs -->
+                        </div><!-- .col -->
+                        <div class="col-sm-4 col-lg-2">
+                            <div class="wgs wgs-menu">
+                                <h6 class="wgs-title">Important Link</h6>
+                                <ul class="wgs-links">
+                                    <li><a href="#">WHO Website</a></li>
+                                    <li><a href="#">CDC Website</a></li>
+                                    <li><a href="#">NHS Website</a></li>
+                                    <li><a href="#">Harvard Health</a></li>
+                                </ul>
+                            </div><!-- .wgs -->
+                        </div><!-- .col -->
+                    </div><!-- .row -->
+                </div><!-- .nk-footer-top -->
+                <div class="nk-footer-bottom">
+                    <div class="row align-items-center">
+                        <div class="col-md-6">
+                            <p class="nk-copyright">&copy; 2020 KOVID-19. Template Made by <a href="https://softnio.com">Softnio</a>.</p>
+                        </div><!-- .col -->
+                        <div class="col-md-6">
+                            <ul class="nk-footer-links justify-content-md-end">
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div><!-- .col -->
+                    </div><!-- .row -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="nk-dislaimer">
+                                <p>Disclaimer: We hope you find the information presented on this website useful. This website is for general information and raise awareness of (2019-nCoV) only. <br class="d-none d-lg-block">
+                                    All the information based on WHO, NHS and CDC website.  Information on our website is meant for awareness, if you have any doubt please verify from respective site.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- .nk-footer-bottom -->
+            </div><!-- .container -->
+        </section><!-- .section -->
+    </footer><!-- .nk-footer -->
+</template>

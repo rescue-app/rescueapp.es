f24a79cd21cf97e40543a3032eba1deec9cfb6f5
Chrisimir <chrislund97@gmail.com>
2020-04-03 18:33:22 +0200

    center logo in infographic and added other possible layouts in comments

--- components/infographicSection.vue
+++ components/infographicSection.vue
@@ -1,8 +1,9 @@
 <template>
     <b-container>
         <div class="section-content">
+            <!-- Wrap columns in mobile -->
             <div class="row g-gs justify-content-center flex-lg-nowrap">
-                <div class="d-none d-lg-block col-lg-2 col-xl-3 align-self-end">
+                <div class="d-none d-lg-block col-lg-2 col-xl-3 align-self-center">
                     <div class="protect-block-gfx">
                         <img src="~/assets/images/horizontal-logo.png" alt="" />
                     </div>
@@ -14,6 +15,26 @@
                     <img src="~/assets/images/what-we-do/infografia-oferta.png" alt="" />
                 </div>
             </div>
+
+            <!-- No mobile wrap version -->
+            <!-- <div class="row g-gs justify-content-center flex-nowrap">
+                <div class="col-5 col-lg-4 p-0">
+                    <img src="~/assets/images/what-we-do/infografia-demanda.png" alt="" />
+                </div>
+                <div class="col-2 p-0 pr-1 align-self-center">
+                    <img src="~/assets/images/horizontal-logo.png" alt="" />
+                </div>
+                <div class="col-5 col-lg-4 p-0">
+                    <img src="~/assets/images/what-we-do/infografia-oferta.png" alt="" />
+                </div>
+            </div> -->
+
+            <!-- One image version -->
+            <!-- <div class="row justify-content-center p-0">
+                <div class="align-self-center col-12 col-md-9">
+                    <img src="~/assets/images/what-we-do/infografia-full.jpeg" alt="" />
+                </div>
+            </div> -->
         </div>
     </b-container>
 </template>

6381bdbdac81c84322477b153e8f3404ca2ae3c7
Chrisimir <chrislund97@gmail.com>
2020-04-03 21:48:06 +0200

    made infographic no-wrap in mobile

--- components/infographicSection.vue
+++ components/infographicSection.vue
@@ -2,7 +2,7 @@
     <b-container>
         <div class="section-content">
             <!-- Wrap columns in mobile -->
-            <div class="row g-gs justify-content-center flex-lg-nowrap">
+            <!-- <div class="row g-gs justify-content-center flex-lg-nowrap">
                 <div class="d-none d-lg-block col-lg-2 col-xl-3 align-self-center">
                     <div class="protect-block-gfx">
                         <img src="~/assets/images/horizontal-logo.png" alt="" />
@@ -14,7 +14,7 @@
                 <div class="col-10 col-mb-6 col-sm-9 col-lg-5 col-xl-4 flex-grow-1 ">
                     <img src="~/assets/images/what-we-do/infografia-oferta.png" alt="" />
                 </div>
-            </div>
+            </div> -->
 
             <!-- No mobile wrap version -->
             <!-- <div class="row g-gs justify-content-center flex-nowrap">
@@ -30,11 +30,11 @@
             </div> -->
 
             <!-- One image version -->
-            <!-- <div class="row justify-content-center p-0">
+            <div class="row justify-content-center p-0">
                 <div class="align-self-center col-12 col-md-9">
                     <img src="~/assets/images/what-we-do/infografia-full.jpeg" alt="" />
                 </div>
-            </div> -->
+            </div>
         </div>
     </b-container>
 </template>

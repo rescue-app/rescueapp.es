1f151d1b5776ffc76b2b920b00ef7a8f9590f707
José Gilberto Mullor <gilber@gmail.com>
2020-04-03 21:23:39 +0200

    Fix iframe from frenalacurva

--- components/mapFrenaLaCurva.vue
+++ components/mapFrenaLaCurva.vue
@@ -5,7 +5,29 @@
                 <h4 class="title">¿Dónde? <br class="d-sm-none"> Toda España</h4>
                 <a href="https://frenalacurva.net/" class="btn btn-sm btn-transparent scrollto"><span>Con la colaboración de FrenaLaCurva</span><em class="icon ni ni-arrow-long-right"></em></a>
             </div>
-            <iframe width="100%" height="600" src="https://es.mapa.frenalacurva.net/views/map" frameborder="0" allowfullscreen></iframe>
+            <div v-if="interaction">
+                <iframe width="100%" height="600" src="https://es.mapa.frenalacurva.net/views/map" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <div v-if="!interaction">
+                <img class="img-fluid d-none d-lg-block" src="~/assets/images/frena-la-curva-iframe.jpg" @click="enableInteraction" />
+                <img class="img-fluid d-lg-none" src="~/assets/images/frena-la-curva-iframe-mobile.jpg" @click="enableInteraction" />
+            </div>
         </div>
     </section>
 </template>
+
+<script>
+export default {
+    data () {
+        return {
+            interaction: false
+        }
+    },
+    methods: {
+        enableInteraction () {
+            this.interaction = true
+        }
+    }
+
+}
+</script>

ee971616f663450fe9e3dd16fe3e9555540e0dd1
Chrisimir <chrislund97@gmail.com>
2020-04-03 03:53:08 +0200

    resize for visibility

--- components/infographicSection.vue
+++ components/infographicSection.vue
@@ -7,10 +7,10 @@
                         <img src="~/assets/images/horizontal-logo.png" alt="" />
                     </div>
                 </div>
-                <div class="col-7 col-mb-6 col-sm-7 col-lg-5 col-xl-4 flex-grow-1 order-lg-first">
+                <div class="col-10 col-mb-6 col-sm-9 col-lg-5 col-xl-4 flex-grow-1 order-lg-first">
                     <img src="~/assets/images/what-we-do/infografia-demanda.png" alt="" />
                 </div>
-                <div class="col-7 col-mb-6 col-sm-7 col-lg-5 col-xl-4 flex-grow-1 ">
+                <div class="col-10 col-mb-6 col-sm-9 col-lg-5 col-xl-4 flex-grow-1 ">
                     <img src="~/assets/images/what-we-do/infografia-oferta.png" alt="" />
                 </div>
             </div>

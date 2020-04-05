6ee9f9e99c0b7a93ccade1c26cc65fd7e854f06e
José Gilberto Mullor <gilber@gmail.com>
2020-04-05 16:07:09 +0200

    Fix GA plugin

--- plugins/ga.js
+++ plugins/ga.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import vueAnalytics from 'vue-analytics'
 
-export default ({ app }) => {
-    const enabled = app.$cookies.consent && Array.isArray(app.$cookies.enabledList) && app.$cookies.enabledList.includes('google-analytics')
+export default (ctx, inject) => {
+    const enabled = ctx.app.$cookies.consent && Array.isArray(ctx.app.$cookies.enabledList) && ctx.app.$cookies.enabledList.includes('google-analytics')
     Vue.use(vueAnalytics, {
         id: 'UA-162736581-1',
         disabled: !enabled,
@@ -10,6 +10,9 @@ export default ({ app }) => {
             enabled: false,
             trace: true,
             sendHitTask: true
-        }
+        },
+        router: ctx.app.router
     })
+    ctx.$ga = Vue.$ga
+    inject('ga', Vue.$ga)
 }

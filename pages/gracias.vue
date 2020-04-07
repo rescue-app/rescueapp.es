e099bd12e369d0c0595067d262534405664c11e9
José Gilberto Mullor <gilber@gmail.com>
2020-04-07 17:24:47 +0200

    Add thankyou page

---
+++ pages/gracias.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <SmallHero />
+        <b-container class="mt-4">
+            <b-row>
+                <b-col>
+                    <h2>¡Gracias por participar!</h2>
+                    <h6>Nos pondremos en contacto contigo lo antes posible. Recuerda seguirnos en nuestras redes sociales =)</h6>
+                </b-col>
+            </b-row>
+        </b-container>
+        <Contact />
+    </div>
+</template>
+
+<script>
+import SmallHero from '@/components/smallHero'
+import Contact from '@/components/contact'
+export default {
+    components: {
+        SmallHero,
+        Contact
+    }
+}
+</script>

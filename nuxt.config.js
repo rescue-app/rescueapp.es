53577bc37b346688200240094b58d678c66d5a57
José Gilberto Mullor <gilber@gmail.com>
2020-04-08 00:24:00 +0200

    Fix #1

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,7 +79,7 @@ module.exports = {
         */
         extend (config, ctx) {
         },
-        parallel: true,
+        parallel: false,
         hardSource: true,
         cache: true
     },

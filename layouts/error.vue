ff0b38964b0ddcc35ce3eff4c9acb7e872a17d26
Chrisimir <chrislund97@gmail.com>
2020-04-08 20:20:24 +0200

    added error page

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="pt-5">
+        <div class="text-center p-5">
+            <h1 class="p-4">-404-</h1>
+            <h3 class="p-3">No encuentro nada por aquí :-)</h3>
+            <b-button style="font-size:1.5em;" class="mt-5 mb-5 p-4" squared size="lg" to="/">Volver al inicio</b-button>
+        </div>
+        <div style="margin-bottom:-27px;" class="card bg-dark pb-1 flex-row justify-content-center">
+            <h4 class="p-3">¿Un error fastidioso? <br> Contacta con nosostros</h4>
+            <b-button squared size="lg" to="/#contacto">Contacto</b-button>
+        </div>
+    </div>
+</template>
